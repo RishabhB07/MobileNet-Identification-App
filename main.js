af056c86ae71ddf66a1c9920f2b13be4201c2326
RishabhB07 <73791841+RishabhB07@users.noreply.github.com>
2021-06-24 17:34:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+Webcam.set({
+    width: 310,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90,
+    constrains: {
+        facingMode: "environment"
+    }
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
